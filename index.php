cfa6c3047a57a32f401c868c07db1933291feb06
champdemars <mannings@videotron.ca>
2021-09-01 19:50:07 -0400

    Update index.php
    s

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test page</title>
  </head>
  <body>
- <?php echo '<p>Bonjour la formation 2021-09-01 version 2!</p>'; ?> 
+ <?php echo '<p>Bonjour la formation 2021-09-01 version 3!</p>'; ?> 
  </body>
 </html>
